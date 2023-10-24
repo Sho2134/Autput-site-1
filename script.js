cffbeef6bc724bc7a4d34ff20555cfd98d34684a
Sho2134 <136536205+Sho2134@users.noreply.github.com>
2023-10-24 09:05:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.getElementById('honyaku').addEventListener('click', () => {
+    document.getElementById('apple').textContent = 'apple';
+    document.getElementById('apple').classList.add('app')
+    
+})
